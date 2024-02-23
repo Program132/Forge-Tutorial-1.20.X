2bff502b5d646ef754dd7e2ccc6263acfccce3e7
Program <67320703+Program132@users.noreply.github.com>
2024-02-23 23:50:01 +0100

    Added "tooltips" (like a description for an item/block)

--- src/main/java/fr/program/tutomod/block/custom/SoundBlock.java
+++ src/main/java/fr/program/tutomod/block/custom/SoundBlock.java
@@ -1,16 +1,23 @@
 package fr.program.tutomod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties properties) {
@@ -21,5 +28,11 @@ public class SoundBlock extends Block {
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS, 1f, 1f);
         return InteractionResult.SUCCESS; // return that the right clickign anim is good
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("Making a beautiful sound when right clicked"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 }
--- src/main/java/fr/program/tutomod/item/custom/FuelItem.java
+++ src/main/java/fr/program/tutomod/item/custom/FuelItem.java
@@ -1,9 +1,14 @@
 package fr.program.tutomod.item.custom;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class FuelItem extends Item {
     private int burnTime;
@@ -16,5 +21,13 @@ public class FuelItem extends Item {
     @Override
     public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
         return this.burnTime;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable(
+                "tooltip.tutomod.big_coal.tooltip"
+        ));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
