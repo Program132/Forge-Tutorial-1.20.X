8e929fb01861bc14c09dfc2ef4a3276f2363eff0
Program <67320703+Program132@users.noreply.github.com>
2024-02-23 21:49:45 +0100

    Adding textures to Sapphire Ore blocks

--- src/main/java/fr/program/tutomod/item/ModCreativeTabs.java
+++ src/main/java/fr/program/tutomod/item/ModCreativeTabs.java
@@ -34,6 +34,10 @@ public class ModCreativeTabs {
                         // Accepting blocks from the mod
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
                     })
                     .build()); // Set icon of the tab to Sapphire
 }
