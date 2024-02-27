10890eaf3bf654ccf402363e7e71c54dd843fef4
Program <67320703+Program132@users.noreply.github.com>
2024-02-27 14:57:50 +0100

    ignore

--- src/main/java/fr/program/tutomod/item/custom/MetalDetectorItem.java
+++ src/main/java/fr/program/tutomod/item/custom/MetalDetectorItem.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class MetalDetectorItem extends Item {
